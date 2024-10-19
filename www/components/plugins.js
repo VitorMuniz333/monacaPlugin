0512069edd113ddcdd6612c8afae531c90215cc2
VitorMuniz333 <giftpliss1.8@gmail.com>
2024-10-19 10:51:44 +0900

    blabla

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,13 +1,26 @@
-// This is a JavaScript file
-
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    console.log(navigator.notification);
-}
 
 $(document).on("click","#alerta", function(){
 
-    function retorno(){
+     function alertDismissed(){
     }
-    navigator.notification.alert("aleatório", retorno,"Aviso!","alerta");
+    navigator.notification.alert("aleatório", alertDismissed,"Aviso!","alerta");
+});
+
+$(document).on("click","#confirm", function(){
+    function confirma(buttonIndex){
+        if(buttonIndex == "1"){     
+        navigator.notification.alert("Escolheu a opção A");
+        }else{
+        navigator.notification.alert("Escolheu a opção B");
+        }
+    }
+    navigator.notification.confirm("Escolha A ou B", confirma,"Escolha",['A','B']);
+});
+
+$(document).on("click","#beep", function(){
+    navigator.notification.beep(4);
+});
+
+$(document).on("click","#vibra", function(){
+    navigator.vibrate([1000, 1000, 3000, 1000, 5000]);
 });
