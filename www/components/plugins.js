d929e5d5a362f87298418919c144c2f538b56df6
VitorMuniz333 <giftpliss1.8@gmail.com>
2024-10-19 09:59:59 +0900

    sei lá cara...

---
+++ www/components/plugins.js
@@ -0,0 +1,13 @@
+// This is a JavaScript file
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.notification);
+}
+
+$(document).on("click","#alerta", function(){
+
+    function retorno(){
+    }
+    navigator.notification.alert("aleatório", retorno,"Aviso!","alerta");
+});
